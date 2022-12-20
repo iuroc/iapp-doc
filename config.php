8ae88532017fecf9478ad8ff936479b0e35ec2b6
oyps <ponconsoft@gmail.com>
2022-12-20 13:11:17 +0800

    数据库配置

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 配置类
+ */
+class Config
+{
+    /**
+     * 数据库配置信息
+     */
+    public static array $mysql = [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => '12345678',
+        'db' => 'ponconsoft'
+    ];
+    /**
+     * 数据表信息
+     */
+    public static array $table = [
+        'book' => 'iapp_doc_book',
+        'article' => 'iapp_doc_article',
+        'into' => 'iapp_doc_into'
+    ];
+}
