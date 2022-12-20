21db131c6ddb8017bf7e3ebb936cff0483f0d212
oyps <ponconsoft@gmail.com>
2022-12-20 16:19:46 +0800

    新增公共CSS

--- head.php
+++ head.php
@@ -4,3 +4,22 @@
 <link rel="stylesheet" href="https://cdn.staticfile.org/bootstrap/5.2.3/css/bootstrap.min.css" />
 <script src="https://cdn.staticfile.org/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
 <!-- <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script> -->
+<style>
+    .limit-line-4 {
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-line-clamp: 4;
+        -webkit-box-orient: vertical;
+    }
+
+    .text-justify {
+        text-align: justify;
+    }
+</style>
+<script>
+    window.onload = function() {
+        document.body.ondragstart = () => {
+            return false
+        }
+    }
+</script>
