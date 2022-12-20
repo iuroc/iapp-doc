808c6ab61880cafb6cde6af5ddcf4d4d8d3db41a
oyps <ponconsoft@gmail.com>
2022-12-20 16:28:19 +0800

    关于页面

--- about.php
+++ about.php
@@ -21,7 +21,13 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mx-auto">
-                <h3 class="mb-3">关于</h3>
+                <div class="h3 mb-3">关于</div>
+                <ul>
+                    <li class="mb-1">Github：<a href="https://github.com/oyps/iapp-doc" target="_blank">https://github.com/oyps/iapp-doc</a></li>
+                    <li class="mb-1">作者：欧阳鹏</li>
+                    <li class="mb-1">官网：<a href="https://apee.top" target="_blank">https://apee.top</a></li>
+                    <li>开发日期：2022年12月19日</li>
+                </ul>
             </div>
         </div>
     </div>
