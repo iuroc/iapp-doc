88b60782cdc6340b8abdc2ed184fcab99e95b026
oyps <ponconsoft@gmail.com>
2022-12-20 20:57:22 +0800

    新增文章页面显示

--- article.php
+++ article.php
@@ -9,6 +9,9 @@ class Article extends Public_fun
 {
     public function __construct()
     {
+        $this->get_article_id();
+        $this->get_article_info();
+        $this->get_book_info();
     }
     public function get_article_id()
     {
@@ -17,6 +20,16 @@ class Article extends Public_fun
             die('手册ID不能为空');
         }
         $this->article_id = $article_id;
+    }
+    public function get_article_info()
+    {
+        $table = Config::$table['article'];
+        $sql = "SELECT * FROM `$table` WHERE `id` = {$this->article_id};";
+        $result = mysqli_query(Init::$conn, $sql);
+        $article_info = mysqli_fetch_assoc($result);
+        $this->article_info = $article_info;
+        $this->book_id = $article_info['book_id'];
+        $this->article_title = $article_info['title'];
     }
 }
 $article = new Article();
@@ -27,6 +40,9 @@ $article = new Article();
 <head>
     <?php require('./head.php') ?>
     <title><?php echo $article->article_title ?> - iApp 手册网</title>
+    <script src="https://cdn.staticfile.org/marked/4.2.4/marked.min.js"></script>
+    <meta name="description" content="<?php echo str_replace("\n", '', htmlentities(mb_substr($article->article_info['content'], 0, 200))) ?>">
+    <link rel="stylesheet" href="css/prism.css">
     <script>
         const PAGE_NAME = 'book' // 页面标识
     </script>
@@ -35,8 +51,33 @@ $article = new Article();
 <body>
     <?php require('./nav.php') ?>
     <div class="container">
-        <div class="h4 mb-3"><?php echo $article->article_title ?></div>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="./" class="text-decoration-none">主页</a></li>
+                <li class="breadcrumb-item"><a href="./book.php?book_id=<?php echo $article->book_info['id'] ?>" class="text-decoration-none"><?php echo $article->book_info['title'] ?></a></li>
+                <li class="breadcrumb-item active"><?php echo $article->article_title ?></li>
+            </ol>
+        </nav>
+        <h4 class="mb-3"><?php echo $article->article_title ?></h4>
+        <div id="content" class="mb-3"></div>
+        <div class="mb-4">
+            <a class="btn btn-outline-primary btn-sm me-2" href="edit.php?id=<?php echo $article->article_info['id'] ?>">编辑</a>
+            <button class="btn btn-outline-success btn-sm me-2">复制</button>
+            <a class="btn btn-outline-danger btn-sm" href="delete.php?id=<?php echo $article->article_info['id'] ?>">删除</a>
+        </div>
     </div>
+    <script>
+        <?php
+        function parse_print($text)
+        {
+            $text = htmlentities($text);
+            return json_encode(['text' => $text]);
+        }
+        ?>
+        let data = <?php echo parse_print($article->article_info['content']) . PHP_EOL ?>
+        document.getElementById('content').innerHTML = marked.parse(data.text);
+    </script>
+    <script src="js/prism.js"></script>
 </body>
 
 </html>
