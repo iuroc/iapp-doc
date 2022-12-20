8a9103256e2e11ee510c8d807687da6d20ca6ad3
oyps <ponconsoft@gmail.com>
2022-12-20 13:11:09 +0800

    关于页

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * 上传手册
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <?php include './head.php' ?>
+    <title>关于</title>
+    <script>
+        const PAGE_NAME = 'about' // 页面标识
+    </script>
+</head>
+
+<body>
+    <?php include './nav.php' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto">
+                <h3 class="mb-3">关于</h3>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
