6bb166c000cc2b7d5b9300dbcfc89ff49df75e10
oyps <ponconsoft@gmail.com>
2022-12-22 12:57:35 +0800

    增加网站图标

--- head.php
+++ head.php
@@ -3,7 +3,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.staticfile.org/bootstrap/5.2.3/css/bootstrap.min.css" />
 <script src="https://cdn.staticfile.org/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
-<!-- <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script> -->
+<link rel="apple-touch-icon" sizes="180x180" href="icon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png">
+<link rel="manifest" href="./site.webmanifest">
 <style>
     .limit-line-4 {
         overflow: hidden;
