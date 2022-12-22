332e570c6c6f03e83bfe4ea79ea091e720c6421f
oyps <ponconsoft@gmail.com>
2022-12-22 17:12:59 +0800

    优化数据库导出

--- output.php
+++ output.php
@@ -7,14 +7,26 @@ require('./public_fun.php');
  */
 class Output extends Public_fun
 {
+    /**
+     * 指令类型
+     */
+    public string $action;
     public function __construct()
     {
-        if (!file_exists('sql_output')) {
-            mkdir('sql_output');
-        }
         $this->must_login();
-        $this->output_table(Config::$table['book']);
-        $this->output_table(Config::$table['article']);
+        $this->action = $_GET['action'] ?? '';
+        if ($this->action == 'output') {
+            if (!file_exists('sql_output')) {
+                mkdir('sql_output');
+            }
+            $this->output_table(Config::$table['book']);
+            $this->output_table(Config::$table['article']);
+            header('location: output.php?action=success');
+            die();
+        } else if ($this->action == '' && file_exists('sql_output/' . Config::$table['book'] . '.sql') && file_exists('sql_output/' . Config::$table['article'] . '.sql')) {
+            // 检查数据库文件是否存在
+            $this->action = 'exists';
+        }
     }
     /**
      * 导出数据表
@@ -29,7 +41,7 @@ class Output extends Public_fun
     }
 }
 
-new Output();
+$output = new Output();
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -45,8 +57,20 @@ new Output();
 <body>
     <?php require('./nav.php') ?>
     <div class="container">
-        <a class="btn btn-primary me-2" href="sql_output/<?php echo Config::$table['book'] ?>.sql">手册数据</a>
-        <a class="btn btn-success" href="sql_output/<?php echo Config::$table['article'] ?>.sql">文章数据</a>
+        <?php
+        if ($output->action == 'success') {
+            echo '<div class="h5 text-success mb-3">导出成功！</div>';
+        }
+        if ($output->action == 'output' || $output->action == 'success' || $output->action == 'exists') {
+            echo '
+        <a class="btn btn-primary me-2" href="sql_output/' . Config::$table['book'] . '.sql">手册数据</a>
+        <a class="btn btn-success me-2" href="sql_output/' . Config::$table['article'] . '.sql">文章数据</a>
+        <a class="btn btn-secondary" href="?action=output">重新导出</a>';
+        } else {
+            echo '
+        <a class="btn btn-primary" href="?action=output">开始导出</a>';
+        }
+        ?>
     </div>
     <?php require('./footer.php') ?>
 </body>
