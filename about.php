3122133e78c482b938adaf893a09afe7dc6e4e04
oyps <ponconsoft@gmail.com>
2022-12-23 16:35:23 +0800

    完善关于页

--- about.php
+++ about.php
@@ -25,11 +25,13 @@ require('./include/init.php');
             <li class="breadcrumb-item active">关于</li>
         </ol>
         <div class="h3 mb-3">关于</div>
+        <p>这是一个专注于分享 iApp 开发技术的网站，由热爱 iApp 的开发者创建。</p>
+        <p>iApp 是一代程序员的青春，我们缅怀，也在不断前行。</p>
+        <p>本站长期收集优质教程文档，欢迎您通过下方的邮箱向本站投稿。</p>
         <ul>
-            <li class="mb-1">Github：<a href="https://github.com/oyps/iapp-doc" target="_blank">https://github.com/oyps/iapp-doc</a></li>
-            <li class="mb-1">作者：欧阳鹏</li>
-            <li class="mb-1">官网：<a href="https://apee.top" target="_blank">https://apee.top</a></li>
-            <li>开发日期：2022年12月19日</li>
+            <li class="mb-1">投稿、反馈邮箱：<a href="mailto:m@apee.top" class="text-decoration-none">m@apee.top</a></li>
+            <li class="mb-1">站长的 Github：<a href="https://github.com/oyps" class="text-decoration-none" target="_blank">欧阳鹏（oyps）</a></li>
+            <li class="mb-1">站长的工作室：<a href="https://apee.top" class="text-decoration-none" target="_blank">https://apee.top</a></li>
         </ul>
     </div>
     <?php require('./include/footer.php') ?>
