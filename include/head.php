5bfd5639ce7a9e3a2ccb97f9b6d0a52623bb3139
欧阳鹏 <61752998+oyps@users.noreply.github.com>
2022-12-28 03:31:47 +0800

    Update head.php

--- include/head.php
+++ include/head.php
@@ -46,3 +46,12 @@
         }
     }
 </script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0c0640f661e855335fc5dc6585e8247c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
